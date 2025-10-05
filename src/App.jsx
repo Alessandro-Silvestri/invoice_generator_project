8e6f8467f7af76f8a61766917910b1ebc7e1006e
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2025-10-05 12:53:14 +0100

    design of a items row and test being responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import TryComponent from "./components/TryComponent";
 
 function App() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl text-amber-400 font-bold">Hello world!</h1>
+      <div className="flex flex-wrap gap-2">
+        <div className="w-full sm:w-14 flex-none bg-amber-700">01</div>
+
+        <div className="w-1/2 sm:w-64 flex-1 bg-blue-500">02</div>
+        <div className="w-1/2 sm:w-32 flex-1 bg-amber-700">03</div>
+      </div>
     </div>
   );
 }
